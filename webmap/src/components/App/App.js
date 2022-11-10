0661efc3d8fda77f6603e9d68bab38bca6a50569
Viet Nguyen <vh.nguyen@utas.edu.au>
2022-11-10 16:28:29 +1100

    First push

---
+++ webmap/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "../Map/Map";
+
+function App() {
+  return (
+      <React.Fragment>
+        <Map/>
+      </React.Fragment>
+  )
+}
+
+export default App;
